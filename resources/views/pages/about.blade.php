170a729e9845a05240f61f7f5f8a43e6af115464
Anupam Shukla <shuklaanupam18@gmail.com>
2019-01-08 15:36:53 +0530

    Basic routing and controllers

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name','LSAPP')}}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    
+    </head>
+    <body>
+    <h1>This is about page</h1>
+    <p>Some random text here</p>
+    </body>
+</html>
